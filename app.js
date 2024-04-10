14a9111969890e5df7c6e8e815926fcb755881f5
Felix Hoops <9974641+jfelixh@users.noreply.github.com>
2024-04-10 14:44:51 +0200

    Working Savepoint

--- app.js
+++ app.js
@@ -1,102 +1,113 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var passport = require('passport');
-var { Strategy } = require('passport-openidconnect');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
+var passport = require("passport");
+//var { Strategy } = require("passport-openidconnect");
+var { Strategy: OidcStrategy, Issuer, Client } = require("openid-client");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(session({
-  secret: 'MyVoiceIsMyPassportVerifyMe',
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "MyVoiceIsMyPassportVerifyMe",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 // set up passport
-passport.use('oidc', new Strategy({
-  issuer: '{OKTA_OAUTH2_ISSUER}',
-  authorizationURL: 'https://{OKTA_DOMAIN}/oauth2/default/v1/authorize',
-  tokenURL: 'https://{OKTA_DOMAIN}/oauth2/default/v1/token',
-  userInfoURL: 'https://{OKTA_DOMAIN}/oauth2/default/v1/userinfo',
-  clientID: '{OKTA_OAUTH2_CLIENT_ID}',
-  clientSecret: '{OKTA_OAUTH2_CLIENT_SECRET}',
-  callbackURL: 'http://localhost:3000/authorization-code/callback',
-  scope: 'openid profile'
-}, (issuer, profile, done) => {
-  return done(null, profile);
-}));
+Issuer.discover("http://localhost:5004").then((bridgeIssuer) => {
+  const client = new bridgeIssuer.Client({
+    client_id: "109c8503-6b5e-4b90-a32e-4244165095a3",
+    client_secret: "some-secret",
+    redirect_uris: ["http://localhost:3000/authorization-code/callback"],
+    token_endpoint_auth_method: "client_secret_post",
+  });
+  passport.use(
+    "oidc",
+    new OidcStrategy({ client }, (tokenSet, userinfo, done) => {
+      // Save user profile or perform other operations
+      console.log("LOGIN SUCCESS");
+      console.log(tokenSet);
+      console.log(userinfo);
+      return done(null, { displayName: userinfo.sub });
+    })
+  );
 
-passport.serializeUser((user, next) => {
-  next(null, user);
-});
+  passport.serializeUser((user, next) => {
+    next(null, user);
+  });
 
-passport.deserializeUser((obj, next) => {
-  next(null, obj);
-});
+  passport.deserializeUser((obj, next) => {
+    next(null, obj);
+  });
 
-function ensureLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
+  function ensureLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+
+    res.redirect("/login");
   }
 
-  res.redirect('/login')
-}
+  app.use("/", indexRouter);
+  app.use("/users", usersRouter);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+  app.use("/login", passport.authenticate("oidc"));
 
-app.use('/login', passport.authenticate('oidc'));
+  // not using the passport redirect options and instead adding another handler seems to cause errors
+  app.use(
+    "/authorization-code/callback",
+    passport.authenticate("oidc", {
+      failureRedirect: "/error",
+      successRedirect: "/profile",
+    })
+  );
 
-app.use('/authorization-code/callback',
-  passport.authenticate('oidc', { failureRedirect: '/error' }),
-  (req, res) => {
-    res.redirect('/profile');
-  }
-);
+  app.use("/profile", ensureLoggedIn, (req, res) => {
+    res.render("profile", { title: "Express", user: req.user });
+  });
 
-app.use('/profile', ensureLoggedIn, (req, res) => {
-  res.render('profile', { title: 'Express', user: req.user });
-});
+  app.get("/logout", (req, res) => {
+    req.logout();
+    req.session.destroy();
+    res.redirect("/");
+  });
 
-app.get('/logout', (req, res) => {
-  req.logout();
-  req.session.destroy();
-  res.redirect('/');
-});
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  });
 });
 
 module.exports = app;
